b3ba326c10ec16a6cf41222ef6105c29308c2171
Ashwanijhariya18 <105935195+Ashwanijhariya18@users.noreply.github.com>
2022-06-06 17:30:40 +0530

    compute 3

---
+++ UC4.sh
@@ -0,0 +1,10 @@
+echo "-----------Welcome to sorting problems-------------"
+#uc1
+read -p "Enter a value: " a
+read -p "Enter b value: " b
+read -p "Enter c value: " c
+
+echo "UC1: Values of a=$a, b=$b and c=$c"
+#uc4
+uc4=$((c+a/b))
+echo "UC4: is $uc4"
